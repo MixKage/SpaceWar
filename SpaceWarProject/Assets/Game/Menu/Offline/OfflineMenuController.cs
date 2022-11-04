a54bf140d22b1157302fe7b5518ab584d92db94d
MixKage <shishov265@gmail.com>
2022-11-04 15:30:53 +0300

    add: RandomName and Exeption

--- SpaceWarProject/Assets/Game/Menu/Offline/OfflineMenuController.cs
+++ SpaceWarProject/Assets/Game/Menu/Offline/OfflineMenuController.cs
@@ -1,37 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using kcp2k;
 using Mirror;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class OfflineMenuController : MonoBehaviour
 {
-    private NetworkManager manager;
-    [SerializeField] private TMP_InputField IPInputField;
-    [SerializeField] private TMP_InputField Port;
-    [SerializeField] private TMP_InputField NamePlayer;
-
+    private NetworkManager _manager;
+    [SerializeField] private TMP_InputField ipInputField;
+    [SerializeField] private TMP_InputField port;
+    [SerializeField] private TMP_InputField namePlayer;
+    [SerializeField] private TMP_Text texNotification;
+    [SerializeField] private GameObject errorNotification;
+    private KcpTransport _kcpTransport;
     void Awake()
     {
-        manager = GetComponent<NetworkManager>();
+        _manager = GetComponent<NetworkManager>();
+        _kcpTransport = GetComponent<KcpTransport>();
     }
     
     public void CreateHost()
     {
-        manager.StartHost();
+        try
+        {
+            _manager.networkAddress = ipInputField.text;
+            _kcpTransport.Port = ushort.Parse(port.text);
+            _manager.StartHost();
+        }
+        catch (Exception e)
+        {
+            texNotification.text = e.Message;
+            errorNotification.SetActive(true);
+        }
     }
     
     public void ConnectToIpAddress()
     {
-        manager.networkAddress = IPInputField.text;
-        manager.StartClient();
+        try
+        {
+            _manager.networkAddress = ipInputField.text;
+            _manager.StartClient();
+        }
+        catch (Exception e)
+        {
+            texNotification.text = e.Message;
+            errorNotification.SetActive(true);
+        }
     }
 
     public void CreateServerByIp()
     {
-        manager.networkAddress = IPInputField.text;
-        manager.StartServer();
+        try
+        {
+            _manager.networkAddress = ipInputField.text;
+            _kcpTransport.Port = ushort.Parse(port.text);
+            _manager.StartServer();
+        }
+        catch (Exception e)
+        {
+            texNotification.text = e.Message;
+            errorNotification.SetActive(true);
+        }
     }
 }
